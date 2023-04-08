1023e82a02874d05704bf75ca19b561c8c698e6b
spaceOfSoul <ychh1123@gmail.com>
2023-04-08 23:44:32 +0900

    permmision url 빼고 전부

--- content_script.js
+++ content_script.js
@@ -1,3 +1,36 @@
-window.addEventListener('DOMContentLoaded', function() {
-    console.log("돔 로드")
+const languege = document.querySelector("#language_chosen > a > span");
+
+console.log('execute');
+
+function pasteCodeToCodeMirror(name, code) {
+    chrome.runtime.sendMessage({
+        type: 'pasteCodeToCodeMirror',
+        name: name,
+        code: code
+    });
+}
+
+document.addEventListener('paste', (e) => {
+    e.preventDefault();
+    // console.log(e);
+    // console.log(languege.innerText);
+
+    if (languege.innerText.substr(0, 4) != "Java")
+        return;
+
+    let paste_txt = (e.clipboardData || window.clipboardData).getData("text");
+    txt_list = paste_txt.split('\n');
+    // console.log(txt_list);
+    paste_txt = "";
+    for (let i = 0; i < txt_list.length; i++) {
+        let txt = txt_list[i];
+        if (txt.indexOf('package ') === 0) {
+            continue;
+        } else if (txt.indexOf('public class ') === 0) {
+            txt = "public class Main {";
+        }
+        paste_txt += txt + "\n";
+    }
+    pasteCodeToCodeMirror("source", paste_txt);
+    console.log("?")
 });
